a0fbe1ece57a61dc52a8725cf73dbfd7cff27aa5
Chris <cleecanth@gmail.com>
2018-09-30 02:18:22 -0500

    Add babel and sass, prettierify

---
+++ config/utils.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const sass = require('node-sass');
+
+const babelConfig = {
+    cacheDirectory: true,
+    configFile: false,
+    babelrc: false,
+    ignore: [/@?babel/g, /core-js/g],
+    sourceType: 'unambiguous',
+    auxiliaryCommentBefore: 'Babel»',
+    presets: [
+        [
+            require('@babel/preset-env'),
+            {
+                loose: true,
+                modules: false,
+                useBuiltIns: 'usage',
+                debug: false,
+            },
+        ],
+    ],
+    plugins: [
+        [
+            require('@babel/plugin-transform-runtime'),
+            {
+                helpers: true,
+                corejs: false,
+                loose: true,
+                useESModules: true,
+            },
+        ],
+        [require('@babel/plugin-syntax-dynamic-import')],
+    ],
+};
+
+const sassConfig = {
+    outputStyle: 'expanded',
+    errLogToConsole: true,
+    includePaths: [path.resolve('../src'), path.resolve('../static')],
+};
+
+function processSass(input) {
+    const content = input.content;
+    const attributes = input.attributes;
+    const filename = input.filename;
+
+    if (
+        attributes.type !== 'text/scss' &&
+        attributes.type !== 'text/sass' &&
+        attributes.lang !== 'scss' &&
+        attributes.lang !== 'sass'
+    ) {
+        return;
+    }
+    try {
+        sassConfig.data = content;
+        sassConfig.outFile = filename;
+        sassConfig.includePaths = [path.dirname(filename)];
+        const result = sass.renderSync(sassConfig);
+
+        return {
+            code: result.css.toString('utf-8'),
+            map: result.map,
+        };
+    } catch (e) {
+        console.error(filename + '\n', new Error(e));
+        return;
+    }
+}
+
+module.exports = {
+    babel: babelConfig,
+    sass: processSass
+};
--- webpack.config.js
+++ webpack.config.js
@@ -5,73 +5,123 @@ const pkg = require('./package.json');
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
+const configUtils = require('./config/utils.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-	client: {
-		entry: config.client.entry(),
-		output: config.client.output(),
-		resolve: {
-			extensions: ['.js', '.json', '.html'],
-			mainFields: ['svelte', 'module', 'browser', 'main']
-		},
-		module: {
-			rules: [
-				{
-					test: /\.html$/,
-					use: {
-						loader: 'svelte-loader',
-						options: {
-							dev,
-							hydratable: true,
-							hotReload: true
-						}
-					}
-				}
-			]
-		},
-		mode,
-		plugins: [
-			dev && new webpack.HotModuleReplacementPlugin(),
-			new webpack.DefinePlugin({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-		].filter(Boolean),
-		devtool: dev && 'inline-source-map'
-	},
+    client: {
+        entry: config.client.entry(),
+        output: config.client.output(),
+        resolve: {
+            extensions: ['.js', '.json', '.html'],
+            mainFields: ['svelte', 'module', 'browser', 'main'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: configUtils.babel,
+                        },
+                        {
+                            loader: 'svelte-loader',
+                            options: {
+                                emitCss: true, // pass CSS to webpack
+                                hydratable: true,
+                                hotReload: dev,
+                                dev: dev,
+                                preserveComments: dev,
+                                preprocess: {
+                                    style: configUtils.sass,
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        dev
+                            ? {
+                                  // CSS HMR
+                                  loader: 'style-loader',
+                                  options: { sourceMap: true },
+                              }
+                            : // extract CSS in production builds
+                              MiniCssExtractPlugin.loader,
+                        { loader: 'css-loader', options: { sourceMap: dev } },
+                        // {
+                        //     loader: 'postcss-loader',
+                        //     options: { sourceMap: dev },
+                        // },
+                    ],
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    enforce: 'post',
+                    exclude: [
+                        /node_modules\/babel-/m,
+                        /node_modules\/core-js\//m,
+                        /node_modules\/regenerator-runtime\//m,
+                        /node_modules\/@?babel/,
+                    ],
+                    use: {
+                        loader: 'babel-loader',
+                        options: configUtils.babel,
+                    },
+                },
+            ],
+        },
+        mode,
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[hash]/[name].css',
+                chunkFilename: '[hash]/[name].[id].css',
+            }),
+            dev && new webpack.HotModuleReplacementPlugin(),
+            new webpack.DefinePlugin({
+                'process.browser': true,
+                'process.env.NODE_ENV': JSON.stringify(mode),
+            }),
+        ].filter(Boolean),
+        devtool: dev && 'inline-source-map',
+    },
 
-	server: {
-		entry: config.server.entry(),
-		output: config.server.output(),
-		target: 'node',
-		resolve: {
-			extensions: ['.js', '.json', '.html'],
-			mainFields: ['svelte', 'module', 'browser', 'main']
-		},
-		externals: Object.keys(pkg.dependencies),
-		module: {
-			rules: [
-				{
-					test: /\.html$/,
-					use: {
-						loader: 'svelte-loader',
-						options: {
-							css: false,
-							generate: 'ssr',
-							dev
-						}
-					}
-				}
-			]
-		},
-		mode: process.env.NODE_ENV,
-		performance: {
-			hints: false // it doesn't matter if server.js is large
-		}
-	},
+    server: {
+        entry: config.server.entry(),
+        output: config.server.output(),
+        target: 'node',
+        resolve: {
+            extensions: ['.js', '.json', '.html'],
+            mainFields: ['svelte', 'module', 'browser', 'main'],
+        },
+        externals: Object.keys(pkg.dependencies),
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: {
+                        loader: 'svelte-loader',
+                        options: {
+                            css: false,
+                            generate: 'ssr',
+                            dev,
+                        },
+                    },
+                },
+            ],
+        },
+        mode: process.env.NODE_ENV,
+        performance: {
+            hints: false, // it doesn't matter if server.js is large
+        },
+    },
 
-	serviceworker: {
-		entry: config.serviceworker.entry(),
-		output: config.serviceworker.output(),
-		mode: process.env.NODE_ENV
-	}
+    serviceworker: {
+        entry: config.serviceworker.entry(),
+        output: config.serviceworker.output(),
+        mode: process.env.NODE_ENV,
+    },
 };
